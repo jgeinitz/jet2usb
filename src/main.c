1716ede027e716432919dafac8c13e9e57ef21fd
Juergen <jgeinitz@gmx.de>
2018-09-14 18:00:15 +0200

    new function description

--- src/main.c
+++ src/main.c
@@ -28,10 +28,48 @@ char *me;
  * 
  */
 int main(int ac, char** av) {
-
-#define P_MAX_LEN 64
+    int terminating = 0;
+    
     me = av[0];
 
+    setup();
+    parseCommandline();
+    prepareSockets();
+    while ( !terminating ) {
+        prepareSelect();
+        switch ( performSelect() ) {
+            case 1:
+                readDataFromSocket();
+                break;
+            case 2:
+                readCommandSocket();
+                break;
+            case 3:
+                readDataFromPrinter();
+                break;
+            case 4:
+                writeDataToPrinter();
+                break;
+            case 5:
+                writeDataToSocket();
+                break;
+            case 6:
+                writeCommandToPrinter();
+                break;
+            case 7:
+                writeCommandSocket();
+                break;
+            case 8:
+                acceptDataSocket();
+                break;
+            case 9:
+                acceptCommandSocket();
+                break;
+            default:
+                terminating = terminateRequest();
+                break;
+        }
+    }
     /**********************************************
      * bye
      **********************************************/
