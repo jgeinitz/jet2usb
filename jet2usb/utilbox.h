77c5313bdefda86e38235fc535a9974ac3947094
juergen <jgeinitz@gmx.de>
2020-09-11 11:09:33 +0200

    refact

--- jet2usb/utilbox.h
+++ jet2usb/utilbox.h
@@ -14,21 +14,30 @@ extern "C" {
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int parsearguments(int ac, char **av,
-        int *verbose,
-        int *jetport,
-        int *cmdport,
-        int *testmode,
-        char *printer,
-        int printermaxlen);
+int parsearguments(
+		int    ac,
+		char **av,
+        int   *verbose,
+        int   *jetport,
+        int   *cmdport,
+        int   *testmode,
+        char  *printer,
+        int    printermaxlen);
+
 int testprinter(char *printer, int testmode);
+
 int init_data_listener(int *fds, int port,struct sockaddr_in6 *dataAddress);
+
 int init_cmd_listener(int *fds, int port,struct sockaddr_in *dataAddress);
+
 int talkTo(int sd, int *t, int v);
+
 int copyTo(int from, int to, int verbose, int tst);
 
 void resetGuesser();
+
 void guessJob(char *b, int l);
+
 int isPS();
 
 #ifdef __cplusplus
